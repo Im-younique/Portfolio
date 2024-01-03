218688174a5bf5ba4b73f5ab044a4a24d135df20
Im-younique <dyyim4725@gmail.com>
2024-01-03 20:55:39 +0900

    [feat] base Toggle 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Toggle from "@/components/base/Toggle";
+
 export default function Home() {
   return (
     <main>
+      <Toggle name="test" on={false} />
       <h1>PortPolio</h1>
     </main>
   );
---
+++ src/components/base/Toggle/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import useToggle from "@/hooks/useToggle";
+
+// styles
+import classes from "./Toggle.module.scss";
+
+interface IProps {
+  name: string;
+  on: boolean;
+  disabled?: boolean;
+  onChange?: () => void;
+}
+
+const Toggle = ({ name, on, disabled = false, onChange }: IProps) => {
+  const [checked, toggle] = useToggle(on);
+
+  const handleChange = () => {
+    toggle();
+    onChange && onChange();
+  };
+
+  return (
+    <label className={classes.container}>
+      <input
+        className={classes.input}
+        type="checkbox"
+        name={name}
+        checked={checked}
+        onChange={handleChange}
+        disabled={disabled}
+      />
+      <div className={classes.switch} />
+    </label>
+  );
+};
+
+export default Toggle;
