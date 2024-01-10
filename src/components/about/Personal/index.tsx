a1f9835c10569b97841db0f46ed9eb8c53173298
Im-younique <dyyim4725@gmail.com>
2024-01-10 16:14:06 +0900

    [Add] Personal Info 스타일 적용

--- src/components/about/Personal/index.tsx
+++ src/components/about/Personal/index.tsx
@@ -1,7 +1,63 @@
+import Image from "next/image";
+
+// styles
+import classes from "./personal.module.scss";
+
+// icon
+import {
+  RiUser2Fill,
+  RiInstagramFill,
+  RiLinkedinBoxFill,
+  RiGithubFill,
+} from "@remixicon/react";
+
 export default function Personal() {
   return (
     <div className="content-container">
-      <p>Personal Info</p>
+      <div className={classes.personal_subtitle}>
+        <RiUser2Fill />
+        <h6>Personal Info</h6>
+      </div>
+      <p className={classes.personal_discription}>
+        안녕하세요. 편리한 사용경험과 차별화된 서비스로 세상을 윤택하게
+        만들어가는 프론트엔드 개발자입니다.
+      </p>
+      <div className={classes.information_box}>
+        <Image
+          src={"/myImages/id_photo.jpg"}
+          alt="id_photo"
+          width={117}
+          height={156}
+          className={classes.id_photo}
+        />
+        <ul>
+          <li>
+            <span>이름: </span>
+            <span>임동윤</span>
+          </li>
+          <li>
+            <span>생년월일: </span>
+            <span>1999/09/02</span>
+          </li>
+          <li>
+            <span>핸드폰: </span>
+            <span>010-5863-4725</span>
+          </li>
+          <li>
+            <span>이메일: </span>
+            <span>dyyim4725@gmail.com</span>
+          </li>
+          <li>
+            <span>주소: </span>
+            <span>Cheonan, South Korea (천안)</span>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.link_wrapper}>
+        <RiGithubFill size={35} />
+        <RiLinkedinBoxFill size={35} color="#0A66C2" />
+        <RiInstagramFill size={35} color="#E4405F" />
+      </div>
     </div>
   );
 }
--- src/components/about/Resume/index.tsx
+++ src/components/about/Resume/index.tsx
@@ -1,6 +1,9 @@
+// styles
+import classes from "./resume.module.scss";
+
 export default function Resume() {
   return (
-    <div className="content-container">
+    <div className={`content-container ${classes.resume_container}`}>
       <p>Resume</p>
     </div>
   );
