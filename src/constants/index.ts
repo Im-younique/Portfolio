d8b3fff94f3ee097eac8d36c34c95c1341f4c5a4
Im-younique <dyyim4725@gmail.com>
2024-01-19 18:25:08 +0900

    [Add] 썸네일 이미지 추가 및 리스트 목록 완성

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,46 +4,46 @@ export const PROJECTLIST: IProjectList[] = [
   {
     name: "위킵 Weekip",
     href: "project/weekip",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/weekip.png",
   },
   {
     name: "reDuck",
     href: "project/reduck",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/reduck.png",
   },
   {
     name: "포트폴리오",
     href: "project/portfolio",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/portfolio.png",
   },
   {
     name: "CheerDo",
     href: "project/cheerdo",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/cheer-do.png",
   },
   {
     name: "1kmwine",
     href: "project/1kmwine",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/1kmwine.png",
   },
   {
     name: "달이 들어주는 오늘",
     href: "project/moondiary",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/moon-diary.png",
   },
   {
     name: "Boothstamp",
     href: "project/boothstamp",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/boothstamp.png",
   },
   {
     name: "대흥동 문화의 집",
     href: "project/culture",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/culture.png",
   },
   {
     name: "충남뮤직페스타",
     href: "project/cnmusicfesta",
-    src: "/projectImages/weekip/weekip_main.png",
+    src: "/thumbnail/cnmusic.png",
   },
 ];
