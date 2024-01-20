e34c120ffb444e95008d42b1f51c21aa281bdd45
Im-younique <dyyim4725@gmail.com>
2024-01-20 16:06:04 +0900

    [chore] 제목 부제목 레이아웃 설정

--- src/app/project/[projectName]/layout.tsx
+++ src/app/project/[projectName]/layout.tsx
@@ -1,3 +1,8 @@
+import { PROJECTINFO } from "@/constants";
+
+// icon
+import { RiCalendar2Fill } from "@remixicon/react";
+
 export default function ProjectLayout({
   params,
   children,
@@ -9,7 +14,13 @@ export default function ProjectLayout({
     <div className="detail-wrapper">
       <header className="detail-header">
         <div className="detail-header-txt-wrap">
-          <h3>{params.projectName}</h3>
+          <h3>{PROJECTINFO[params.projectName].title}</h3>
+          <p>
+            <RiCalendar2Fill />
+            <span>프로젝트 수행기간 : </span>
+            {PROJECTINFO[params.projectName].startDate} ~{" "}
+            {PROJECTINFO[params.projectName].endDate}
+          </p>
         </div>
       </header>
       <section className="detail-content-wrapper">
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -47,3 +47,51 @@ export const PROJECTLIST: IProjectList[] = [
     src: "/thumbnail/cnmusic.png",
   },
 ];
+
+export const PROJECTINFO = Object.freeze({
+  weekip: {
+    title: "세상의 흐름을 읽는 IP - Weekip",
+    startDate: "2023/07",
+    endDate: "2023/12",
+  },
+  reduck: {
+    title: "개발자를 위한 커뮤니티 및 멘토링 시스템 - reDuck",
+    startDate: "2023/02",
+    endDate: "(진행중)",
+  },
+  portfolio: {
+    title: "임동윤 포트폴리오",
+    startDate: "2024/01",
+    endDate: "(진행중)",
+  },
+  cheerdo: {
+    title: "너의 TODO를 응원해 - CheerDo",
+    startDate: "2022/12",
+    endDate: "2023/02",
+  },
+  "1kmwine": {
+    title: "가장 빠른 와인 픽 & 오더 플랫폼 - 1kmwine",
+    startDate: "2022/08",
+    endDate: "2022/08",
+  },
+  moondiary: {
+    title: "어른이를 위한 일기장 - 달이 들어주는 오늘",
+    startDate: "2022/06",
+    endDate: "2022/07",
+  },
+  boothstamp: {
+    title: "AI/SW 페스티벌 부스 체험 - BoothStamp",
+    startDate: "2022/07",
+    endDate: "2022/08",
+  },
+  culture: {
+    title: "청소년들의 건강한 문화놀이터 - 대흥동 청소년 문화의 집",
+    startDate: "2022/09",
+    endDate: "2022/11",
+  },
+  cnmusicfesta: {
+    title: "충남 전역에서 펼쳐지는 정열의 음악 축제! - 충남뮤직페스타",
+    startDate: "2021/09",
+    endDate: "2021/12",
+  },
+});
