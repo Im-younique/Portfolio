eef953e5d81cb1ce35b427fd9c33dbf887276bed
Im-younique <dyyim4725@gmail.com>
2024-02-05 18:22:13 +0900

    [Add] 포트폴리오 상세 추가

--- src/app/project/(detail)/constants/index.tsx
+++ src/app/project/(detail)/constants/index.tsx
@@ -31,3 +31,22 @@ export const REDUCKIMAGE = [
     alt: "리덕 게시글 상세_댓글",
   },
 ];
+
+export const PORTFOLIOIMAGE = [
+  {
+    src: "/projectImages/portfolio/profile_main_white.png",
+    alt: "포트폴리오 메인 화이트",
+  },
+  {
+    src: "/projectImages/portfolio/profile_main_black.png",
+    alt: "포트폴리오 메인 블랙",
+  },
+  {
+    src: "/projectImages/portfolio/profile_about.png",
+    alt: "포트폴리오 어바웃",
+  },
+  {
+    src: "/projectImages/portfolio/profile_project.png",
+    alt: "포트폴리오 프로젝트",
+  },
+];
